48412f63078b153f9dbcf429d9bf45a09900238b
unknown <Joilton>
2021-03-20 16:56:34 -0300

    primeiro commit

---
+++ rh-trabalhador/src/main/java/br/com/agarf/rhtrabalhador/RhTrabalhadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.agarf.rhtrabalhador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhTrabalhadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhTrabalhadorApplication.class, args);
+	}
+
+}
