359485a53450a60ba4ed16a6334c132850baba2c
unknown <Joilton>
2021-03-21 14:33:15 -0300

    implementação folha de pagamento

---
+++ folhadepagamento/src/test/java/com/br/agarf/folhadepagamento/FolhadepagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.agarf.folhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhadepagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
